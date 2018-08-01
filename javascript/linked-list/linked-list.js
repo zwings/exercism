d3e6e83480e8d1dccb5bf4f642ed065c3d589a5d
zwings <zwings@hotmail.com>
2018-08-01 19:13:18 +0800

    done

--- javascript/linked-list/linked-list.js
+++ javascript/linked-list/linked-list.js
@@ -1,22 +1,103 @@
-module.exports = LinkedList;
-
 class Node {
-    constructor(addNum){
-        this.currentNum = addNum;
-        this.previousNum = null ;
-        this.nextNum = null ;
-    }
-}
-class LinkedList {
-    constructor (){
-        this.list = {};
-        
-    }
-    push(number){
-        currentNode = new Node;
-        currentNode.currentNum = number;
-        if (LinkedList.list = {}){
-            
-        }
+    constructor(element) {
+        this.element = element;
+        this.previous = null;
+        this.next = null;
     };
 }
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+
+    };
+    push(number) {
+        const node = new Node(number);
+        if (this.head === null) {
+            this.head = node;
+            this.tail = node;
+        } else {
+            var currentNode = this.head;
+            while (currentNode.next) {
+                currentNode = currentNode.next;
+            };
+            currentNode.next = node;
+            node.previous = currentNode;
+            this.tail = node;
+        }
+        this.length++;
+    };
+    pop() {
+        if (this.length > 0) {
+            var currentNode = this.tail;
+            var result = currentNode.element;
+            if (currentNode.previous !== null) {
+                currentNode = currentNode.previous;
+                currentNode.next = null;
+                this.tail = currentNode;
+            } else {
+                this.head = null;
+                this.tail = null;
+            }
+            return result;
+        }
+        this.length--;
+    };
+    shift() {
+        if (this.length > 0) {
+            var currentNode = this.head;
+            var result = currentNode.element;
+            if (currentNode.next !== null) {
+                currentNode = currentNode.next;
+                currentNode.previous = null;
+                this.head = currentNode;
+            } else {
+                this.head = null;
+                this.tail = null;
+            }
+            return result;
+        }
+        this.length--;
+    };
+    unshift(number) {
+        const node = new Node(number);
+        if (this.head === null) {
+            this.head = node;
+            this.tail = node;
+        } else {
+            var currentNode = this.tail;
+            while (currentNode.previous) {
+                currentNode = currentNode.previous;
+            };
+            currentNode.previous = node;
+            node.next = currentNode;
+            this.head = node;
+        }
+        this.length++;
+    };
+    count() {
+        return this.length;
+    };
+    delete(number) {
+        var currentNode = this.head;
+        
+        while (currentNode.element !== number) {
+            currentNode = currentNode.next;
+        };
+        var result = currentNode;
+        if (currentNode.previous !== null && currentNode.next !== null) {
+           currentNode.previous.next = currentNode.next;
+           currentNode.next.previous = currentNode.previous;
+        } else {
+            this.head = null;
+            this.tail = null;
+        }
+        this.length--;
+        return result;
+    }
+
+
+}
+
+module.exports = LinkedList;
